d89ffae3f1a9b54121772f36d4ffac77d9cb3896
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-05-19 12:20:48 -0400

    updates to server.js and created user model

--- config/connection.js
+++ config/connection.js
@@ -5,4 +5,4 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialmed
     useUnifiedTopology: true
 })
 
-mon
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const db = require("./config/connection");
+// routes
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+// route
 
-// db.once("open", () => {
+db.once("open", () => {
     app.listen(PORT, () => {
-        console.log(`API server running on PORT ${PORT}!`)
-    })
-// })
+        console.log(`API server running on PORT ${PORT}!`);
+    });
+});
